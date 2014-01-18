7fd4d0ea6308358bba00ab97482f1b6040a5cf5b
Peter Gao <pgao@MAW.(none)>
2014-01-18 02:59:40 -0800

    oversimplify

--- background.js
+++ background.js
@@ -1,13 +1,9 @@
 function sendLink(tab) {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-        var tabUrl = tabs[0].url;
-        var newUrl = 'http://www.facebook.com/dialog/send?'
-          + 'app_id=713371765353094'
-          + '&link=' + tabUrl
-          + '&redirect_uri=http://instantgratification.herokuapp.com/';
-
-        chrome.tabs.create({url: newUrl});
-    });
+    var newUrl = 'http://www.facebook.com/dialog/send?'
+      + 'app_id=713371765353094'
+      + '&link=' + tab.url
+      + '&redirect_uri=http://instantgratification.herokuapp.com/';
+ 	chrome.tabs.create({url: newUrl});
 }
 
 chrome.browserAction.onClicked.addListener(sendLink);
