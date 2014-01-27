241de801dc4bf998e088a0210ec4eef28dde80ed
Peter Gao <pgao@MAW.(none)>
2014-01-26 22:48:20 -0800

    working original redirect method

--- background.js
+++ background.js
@@ -4,14 +4,6 @@ function sendLink(tab) {
       + '&link=' + tab.url
       + '&redirect_uri=http://instantgratification.herokuapp.com/';
     chrome.tabs.create({url: newUrl});
-
-    chrome.tabs.executeScript(tab.id, {file: "fbinit.js"}, function() {
-        console.log("fbinit executed");
-        chrome.tabs.executeScript(tab.id, {file: "send.js"}, function() {
-            console.log("send.js executed");
-        });
-    });
-
 }
 
 chrome.browserAction.onClicked.addListener(sendLink);
--- send.js
+++
@@ -1,8 +0,0 @@
-console.log("inside send.js");
-    
-FB.ui({
-    method: 'send',
-    link: document.url,
-});
-
-console.log("end of send.js");
